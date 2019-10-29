f2b3578620a686c61f5b22cef4f15f790f0b77ef
Vinayak Redekar <redekar62.vinayak@gmail.com>
2019-10-29 10:29:31 +0800

    Section 2.a,2.b,2.c,2.d

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 ## Section A 
 Optimization in the Animal Class 
 * Not all animals can walk, making Animal class as base for representation of any animal.
-* Since eat() is the basic function any animal can perform, all will inherit this property
+* Since eat() is the basic function any animal can perform, all animal type will inherit this function
 * Segregated the variable behavior like movement, flying etc into different move and fly strategy
-* Optimizing the code to return valued for unit test
+* Optimizing the code to return value for unit test
+
+## Section B
+* Chicken is assumed Domestic, eating patterns vary. 
+* Duck is assumed  assumed to be wild in this example. To segregate domestic and non-domestic.
--- src/com/assigment/modeling/Solution.java
+++ src/com/assigment/modeling/Solution.java
@@ -1,28 +1,43 @@
 package com.assigment.modeling;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 
-
+/*
+ * Basic Animal function
+ */
 class Animal {
 	String eat() {
 		return  "I am eating";	
 	}
 }
 /*
- * Since  animal can walk or crawl or swim
+ * Since  terrestrial animal can walk or crawl
  */
 interface Moveable{
 	String move();
 }
+
 class Walk implements Moveable{
 	public String move() {
 		return "I am walking";
 	}
 }
+
+
+interface Singable {
+	String sing();
+	
+}
+
+/*
+ * Section 1.a & 1.b
+ */
 
 interface FlyBehaviour {
 	String fly();
@@ -54,9 +69,9 @@ class Walking extends Animal {
 }
 
 
-class Bird extends Walking {  // Since all birds can walk but not fly. Flying behaviour will vary 
+class Bird extends Walking implements Singable {  // Since all birds can walk but not fly. Flying behaviour will vary 
 	FlyBehaviour flyBehaviour = new Flying(); // default behaviour and override in case bird doesn't fly in the subclass
-	String sing() {
+	public String sing() {
 		return "I am singing";
 	}
 	
@@ -65,32 +80,128 @@ class Bird extends Walking {  // Since all birds can walk but not fly. Flying be
 	}
 	
 }
+/*
+ * Section 2.a, 2.b , 2.c, 2.d
+ * Chicken inherits Domestic behavior, primarily terrestrial birds with no flying ability
+ * Duck inherits from abstract type Aquatic bird, adding the ability to swim in addition to common bird behaviour
+ * 
+ */
+class DomesticBird extends Bird {
+	
+	DomesticBird(){
+		flyBehaviour = new Flightless();
+	}
+	
+	String eat(){
+		return "I eat only grains!";
+	}
+}
 
+class Chicken extends DomesticBird  {
+	
+	public String sing() {
+		return "Cluck,cluck";
+	}
+}
 
+interface Swimmable{
+	String swim();
+}
+
+class AquaticBird extends Bird implements Swimmable{
+
+	@Override
+	public String swim() {
+		return "I am swimming";
+	}
+	
+}
+
+class Duck extends AquaticBird {
+	
+	public String sing() {
+		return "Quack, quack";
+	}
+}
 
 public class Solution {
 	public static void main(String[] args) {
 		System.out.println("***************************");
 		System.out.println("Basic Bird Behaviour ");
+		System.out.println("***************************");
 		
 		Bird bird = new Bird();
 		System.out.println(bird.eat());
 		System.out.println(bird.walk());
 		System.out.println(bird.sing());
 		System.out.println(bird.fly());
+		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("***************************");
+		System.out.println("Chicken Behaviour ");
+		System.out.println("***************************");
+		
+		Bird chicken = new Chicken();
+		System.out.println(chicken.eat());
+		System.out.println(chicken.walk());
+		System.out.println(chicken.sing());
+		System.out.println(chicken.fly());
+		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("***************************");
+		System.out.println("Duck Behaviour ");
+		System.out.println("***************************");
+		
+		Duck duck = new Duck();
+		System.out.println(duck.eat());
+		System.out.println(duck.walk());
+		System.out.println(duck.sing());
+		System.out.println(duck.fly());
+		System.out.println(duck.swim());
 	}
 	
 	
 	Bird bird;
+	Bird chicken;
+	Duck duck;
+	
 	
 	@Before
 	public void setUp() {
 		bird = new Bird();
+		chicken = new Chicken();
+		duck = new Duck();
 	}
 	
 	@Test
 	public void test_Bird_default_sing() {
 		assertEquals("I am singing", bird.sing());
 	}
+	
+	@Test
+	public void test_Chicken_can_not_swim() {
+		assertFalse(chicken instanceof AquaticBird);
+	}
+	
+	@Test
+	public void test_Duck_can_swim() {
+		assertTrue(duck instanceof AquaticBird);
+		assertEquals("I am swimming", duck.swim());
+	}
+	
+	@Test
+	public void test_Duck_can_Quack() {
+		assertEquals("Quack, quack", duck.sing());
+	}
+	
+	@Test
+	public void test_Chicken_can_Cluck() {
+		assertEquals("Cluck,cluck", chicken.sing());
+	}
+	
 }
 
