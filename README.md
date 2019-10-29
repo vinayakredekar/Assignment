743c6cb46d5e8a8e59db6932a55de91376526965
Vinayak Redekar <redekar62.vinayak@gmail.com>
2019-10-29 09:32:05 +0800

    Commit for Question 1.a & 1.b

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Assignment
+## Section A 
+Optimization in the Animal Class 
+* Not all animals can walk, making Animal class as base for representation of any animal.
+* Since eat() is the basic function any animal can perform, all will inherit this property
+* Segregated the variable behavior like movement, flying etc into different move and fly strategy
+* Optimizing the code to return valued for unit test
--- src/com/assigment/modeling/Solution.java
+++ src/com/assigment/modeling/Solution.java
@@ -1,23 +1,96 @@
 package com.assigment.modeling;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
 class Animal {
-	void walk() {
-		System.out.println("I am walking");
+	String eat() {
+		return  "I am eating";	
+	}
+}
+/*
+ * Since  animal can walk or crawl or swim
+ */
+interface Moveable{
+	String move();
+}
+class Walk implements Moveable{
+	public String move() {
+		return "I am walking";
 	}
 }
 
-class Bird extends Animal {
-	void fly() {
-		System.out.println("I am flying");
+interface FlyBehaviour {
+	String fly();
+}
+
+class Flying implements FlyBehaviour{
+
+	@Override
+	public String fly() {
+		return "I am flying";	
 	}
 }
+
+class Flightless implements FlyBehaviour {
+
+	@Override
+	public String fly() {
+		return "I can not fly";
+	}
+}
+
+
+class Walking extends Animal {
+
+	Moveable moveable = new Walk();//default behaviour terrestrial animals
+	String walk() {
+		return moveable.move();
+	}
+}
+
+
+class Bird extends Walking {  // Since all birds can walk but not fly. Flying behaviour will vary 
+	FlyBehaviour flyBehaviour = new Flying(); // default behaviour and override in case bird doesn't fly in the subclass
+	String sing() {
+		return "I am singing";
+	}
+	
+	String fly() {
+		return flyBehaviour.fly();
+	}
+	
+}
+
+
 
 public class Solution {
 	public static void main(String[] args) {
+		System.out.println("***************************");
+		System.out.println("Basic Bird Behaviour ");
+		
 		Bird bird = new Bird();
-		bird.walk();
-		bird.fly();
-		bird.sing();
+		System.out.println(bird.eat());
+		System.out.println(bird.walk());
+		System.out.println(bird.sing());
+		System.out.println(bird.fly());
+	}
+	
+	
+	Bird bird;
+	
+	@Before
+	public void setUp() {
+		bird = new Bird();
+	}
+	
+	@Test
+	public void test_Bird_default_sing() {
+		assertEquals("I am singing", bird.sing());
 	}
 }
 
